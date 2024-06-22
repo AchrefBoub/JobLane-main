a8cad18cafc8b2a4f8e634f4c611f390708fade5
Achref <159728899+AchrefBoub@users.noreply.github.com>
2024-06-22 09:57:50 +0100

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -58,13 +58,13 @@ export const Home = () => {
             <div className='min-h-screen md:px-20 px-3 pt-14 flex text-white bg-gray-950'>
                 <div className='w-full flex pt-28 flex-col justify-start items-center gap-4'>
                     <div className='flex md:flex-row flex-col items-center justify-center md:gap-10 gap-1'>
-                        <div className='md:text-8xl text-6xl titleT '>دلني</div>
+                        <div className='md:text-8xl text-red-700 text-6xl titleT '>دلني</div>
                         <div className='flex justify-center items-center pt-1'>
                             <Link to="/jobs" className='font-semibold md:text-2xl text-lg blueCol md:py-3 py-2 px-6 md:px-10'>تصفح الوظائف</Link>
                         </div>
                     </div>
                     <div>
-                        <p className='md:text-xl text-sm'>بوابتك <span className='text-yellow-500'>لفرص العمل</span>.</p>
+                        <p className='md:text-xl text-sm'>بوابتك <span className='text-red-700'>دليلك للكسب الطيب</span>.</p>
                     </div>
                     <div className='pt-[8rem] md:px-[1rem] px-[0rem] w-full'>
                         <div className='titleT pb-6 text-2xl'>
